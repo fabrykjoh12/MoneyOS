5093094b95adb51ebafa9f4e17c63745edbd2cf1
fabrykjoh12 <fabrykjoh12@gmail.com>
2026-08-25 16:52:43 +0200

    finance-live: add logout endpoint

---
+++ api/logout.js
@@ -0,0 +1,7 @@
+import { clearSessionCookie } from '../lib/auth.js';
+
+export default async function handler(req, res) {
+  res.setHeader('Cache-Control', 'no-store');
+  res.setHeader('Set-Cookie', clearSessionCookie());
+  return res.status(200).json({ ok: true });
+}
